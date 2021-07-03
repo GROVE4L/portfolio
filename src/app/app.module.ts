d4451748e1d57c952d8008318b158aff1fb1ac85
GROVE4L <grove4l_2013@hotmail.com>
2021-07-03 01:15:20 -0300

    Primer commit - Inicializando el proyecto con el trabajo realizado en local.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    TrabajosComponent
   ],
   imports: [
     BrowserModule,
