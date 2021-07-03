b5ac4934de7295d14c774bef096a044252f4318f
GROVE4L <grove4l_2013@hotmail.com>
2021-07-03 01:29:43 -0300

    Agregadas librerias y variables de firebase para el hosting del portfolio.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDBtiFtrbme7Bhd8phy758rviI_nCokA7o",
+    authDomain: "mi-portfolio-c414a.firebaseapp.com",
+    projectId: "mi-portfolio-c414a",
+    storageBucket: "mi-portfolio-c414a.appspot.com",
+    messagingSenderId: "624464685653",
+    appId: "1:624464685653:web:2411c84ca8da00a46f82dc",
+    measurementId: "G-FSSEMKHHCF"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDBtiFtrbme7Bhd8phy758rviI_nCokA7o",
+    authDomain: "mi-portfolio-c414a.firebaseapp.com",
+    projectId: "mi-portfolio-c414a",
+    storageBucket: "mi-portfolio-c414a.appspot.com",
+    messagingSenderId: "624464685653",
+    appId: "1:624464685653:web:2411c84ca8da00a46f82dc",
+    measurementId: "G-FSSEMKHHCF"
+  }
 };
 
 /*
