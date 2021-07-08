2ceefe6e9ab47edfe696064ed92fea888f3fd112
GROVE4L <grove4l_2013@hotmail.com>
2021-07-08 16:17:22 -0300

    Cambiado icono y colores de icono.

--- src/app/services/proyecto.service.ts
+++ src/app/services/proyecto.service.ts
@@ -7,20 +7,20 @@ export class ProyectoService {
 
   private proyectoList: Array<{nombre:string, descripcion: string, caracteristicas: string[] , imgUrl: string, galeria: string[]}> = [
     { nombre: "Portfolio", 
-    descripcion: "El mismo portfolio forma parte de esta muestra. Es que su misma creación es un ejemplo breve pero consiso de los frecuentes procesos de la programacion: Bucle, array, etc.\nLe invito a observar mi código, así estimará un panorama de mi forma de programar.",
+    descripcion: "El mismo portfolio forma parte de esta muestra. Es que su misma creación es un ejemplo breve pero consiso de los frecuentes procesos de la programación: Bucle, array, etc.\nLe invito a observar mi código, así estimará un panorama de mi forma de programar.",
     caracteristicas: [], 
-    imgUrl:"https://i.ibb.co/jbGYSDG/G.jpg",
+    imgUrl:"../../../assets/img/GLogo.png",
     //galeria: ["https://i.ibb.co/PTDn2nt/Demo.jpg","../../../assets/img/fondoport.png","https://i.ibb.co/CmMt9bT/negocio-Logo.png","https://i.ibb.co/jLzJ26S/WolfST2.png"] },
     galeria: [] },
 
     { nombre: "Juguetería WaWa", 
-    descripcion: "Si juntamos una numerosa cantidad de juguetes y una administracion desorientada, tenemos como resultado una extenuante jornada laboral para los integrantes de esta juguetería. Dicha situación fue lo que llevo al dueño a contactarme.\nEntonces, ¿que le ofrecí?, un sistema que le permite administrar de forma detallada y comprobable cada uno de los movimientos de su negocio.",
+    descripcion: "Si juntamos una numerosa cantidad de juguetes y una administración desorientada, tenemos como resultado una extenuante jornada laboral para los integrantes de esta juguetería. Dicha situación fue lo que llevo al dueño a contactarme.\nEntonces, ¿que le ofrecí?, un sistema que le permite administrar de forma detallada y comprobable cada uno de los movimientos de su negocio.",
     caracteristicas: [],
     imgUrl:"../../../assets/img/Proyecto-2/LogoProyecto.png",
     galeria: [] }, 
 
     { nombre: "Sistema de Restaurant", 
-    descripcion: "Circunstancias recientes han hecho que el sector gastronomico se acerque aun más al internet. Este le ofrece un mundo mas amplio de posibilidades, desde tener una organizacion mas estable en el local hasta un eficiente servicio de delivery.\nY, en este contexto, ofrezco un programa que pueda hacer el trajecto a la digitalidad más comodo y accesible",
+    descripcion: "Circunstancias recientes han hecho que el sector gastronomico se acerque aun más al internet. Este le ofrece un mundo mas amplio de posibilidades, desde tener una organización mas estable en el local hasta un eficiente servicio de delivery.\nY, en este contexto, ofrezco un programa que pueda hacer el trajecto a la digitalidad más comodo y accesible",
     caracteristicas: [], 
     imgUrl:"https://i.ibb.co/PTDn2nt/Demo.jpg",
     galeria: [] },
